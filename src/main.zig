baa3877f994022f754526ac0303850f240099414
luccie-cmd <luccie@sideros.org>
2025-08-04 16:03:33 +0200

    Commented out main stuff

--- src/main.zig
+++ src/main.zig
@@ -18,30 +18,30 @@ pub fn main() !void {
     const allocator = gpa.allocator();
     defer if (gpa.deinit() != .ok) @panic("Leaked memory");
 
-    var global_runtime = mods.GlobalRuntime.init(allocator);
-    defer global_runtime.deinit();
-    try global_runtime.addFunction("debug", mods.Wasm.debug);
+    // var global_runtime = mods.GlobalRuntime.init(allocator);
+    // defer global_runtime.deinit();
+    // try global_runtime.addFunction("debug", mods.Wasm.debug);
 
-    //const file = try std.fs.cwd().openFile("assets/core.wasm", .{});
-    const file = try std.fs.cwd().openFile("./test.wasm", .{});
-    const all = try file.readToEndAlloc(allocator, 1_000_000); // 1 MB
-    defer allocator.free(all);
-    var parser = try mods.Parser.init(allocator, all);
-    defer parser.deinit();
-    parser.parseModule() catch |err| {
-       std.debug.print("[ERROR]: error at byte {x}(0x{x})\n", .{ parser.byte_idx, parser.bytes[parser.byte_idx] });
-       return err;
-    };
-    const module = parser.module();
-    // defer module.deinit(allocator);
+    // //const file = try std.fs.cwd().openFile("assets/core.wasm", .{});
+    // const file = try std.fs.cwd().openFile("./test.wasm", .{});
+    // const all = try file.readToEndAlloc(allocator, 1_000_000); // 1 MB
+    // defer allocator.free(all);
+    // var parser = try mods.Parser.init(allocator, all);
+    // defer parser.deinit();
+    // parser.parseModule() catch |err| {
+    //    std.debug.print("[ERROR]: error at byte {x}(0x{x})\n", .{ parser.byte_idx, parser.bytes[parser.byte_idx] });
+    //    return err;
+    // };
+    // const module = parser.module();
+    // // defer module.deinit(allocator);
 
-    var runtime = try mods.Runtime.init(allocator, module, &global_runtime);
-    defer runtime.deinit(allocator);
+    // var runtime = try mods.Runtime.init(allocator, module, &global_runtime);
+    // defer runtime.deinit(allocator);
 
-    var parameters = [_]mods.VM.Value{.{ .i32 = 17 }};
-    try runtime.callExternal(allocator, .preinit, &parameters);
-    const result = runtime.stack.pop().?;
-    std.debug.print("Result of preinit: {any}\n", .{result});
+    // var parameters = [_]mods.VM.Value{.{ .i32 = 17 }};
+    // try runtime.callExternal(allocator, .preinit, &parameters);
+    // const result = runtime.stack.pop().?;
+    // std.debug.print("Result of preinit: {any}\n", .{result});
 
     //var w = try Renderer.Window.create(800, 600, "sideros");
     //defer w.destroy();
